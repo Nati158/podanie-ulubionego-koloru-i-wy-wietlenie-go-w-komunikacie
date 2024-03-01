b78c2db048f41cd2438b4bb6fb2b9c1cad0b3fe9
Nati158 <141584277+Nati158@users.noreply.github.com>
2024-03-01 07:28:50 +0100

    Add files via upload

---
+++ main37.py
@@ -0,0 +1,6 @@
+def display_favorite_color(color):
+    print("Your favorite color is:", color)
+
+if __name__ == "__main__":
+    color = input("What is your favorite color? ")
+    display_favorite_color(color)
